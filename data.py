54237f4bf665a2fd82fad27a11c819bcbb70af5d
ArataKido <81490192+ArataKido@users.noreply.github.com>
2022-03-23 17:50:49 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,3 @@
-from tkinter.messagebox import QUESTION
-
 
 class Question: 
 
@@ -8,10 +6,21 @@ class Question:
         "Who painted the Mona Lisa": "Leonardo da Vinci",
         "What's a baby rabbit called?": "kittens",
         "Which is the highest grossing Harry Potter film?": "Harry Potter and the Deathly Hallows Part 2'"
-    }
+    }    
+    def __init__(self, question, answer) :
+        self.question=question
+        self.answer=answer
 
-    def __init__(self, question, answer):        
-        self.question = question        
-        self.answer = answer
+    def get_question(self,question):
+        question=self.QUESTIONS
+        for i in question:
+            print(i)
 
-    test=""
+    def get_answer(self, answer):
+        answer=self.QUESTIONS
+        for i in answer:
+            print(answer[i])
+   
+question=Question()
+print(question.get_question())
+print(question.get_answer())
