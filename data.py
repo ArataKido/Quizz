9a7db6cf2c7f7ec817d16dc5440f17f925acf3a0
Shahzod <shahzod20030905@gmail.com>
2022-03-24 16:13:47 +0100

    added main funktion

--- data.py
+++ data.py
@@ -15,14 +15,27 @@ class Question:
         for i in answer:
             print(answer[i])
 
-QUESTIONS={
-    "What is the capital of Finland": "lisabon",
-    "Who painted the Mona Lisa": "Leonardo da Vinci",
-    "What's a baby rabbit called?": "kittens",
-    "Which is the highest grossing Harry Potter film?": "Harry Potter and the Deathly Hallows Part 2'"
-}    
+def main():
+    QUESTIONS={}
+     
 
-question=Question(QUESTIONS,"" )
+    is_on=True
+    while is_on:
+        print("Welcome player.")
+        if input("Do you want to add questions? y/n ").lower()=="y":
+            key=input("Print the question ")
+            value=input("Print the answer ")
+            QUESTIONS.update({key:value})
+            if input("Do you want to add more questions? y/n ").lower()=="y":
+                key_2=input("Print the question ")
+                value_2=input("Print the answer ")
+                QUESTIONS.update({key_2:value_2})
+                print(QUESTIONS)
+            else:
+                break
 
-print(question.get_question())
-print(question.get_answer())
+        
+
+
+if __name__=="__main__":
+    main()
