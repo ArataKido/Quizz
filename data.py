e205d3b235bd69a7ea6a10324644e557cc0b7b38
Shahzod <shahzod20030905@gmail.com>
2022-03-24 23:49:50 +0100

    idk

--- data.py
+++ data.py
@@ -1,24 +1,27 @@
 
-class Question: 
+from re import I
 
+
+class QuestionsForQuiz: 
+    """
+    This class does something but i do not know what it does
+    """
     def __init__(self, question, answer) :
         self.question=question
         self.answer=answer
-
-    def get_question(self,question):
-        question=self.QUESTIONS
-        for i in question:
-            print(i)
-
+    
+    def get_question(self, question):
+        pass
     def get_answer(self, answer):
-        answer=self.QUESTIONS
-        for i in answer:
-            print(answer[i])
+        pass
+        
 
 def main():
-    QUESTIONS={}
-     
+    """
+    This function  gets a questions and answers from the user 
 
+    """
+    
     is_on=True
     while is_on:
         print("Welcome player.")
@@ -27,14 +30,22 @@ def main():
             value=input("Print the answer ")
             QUESTIONS.update({key:value})
             if input("Do you want to add more questions? y/n ").lower()=="y":
-                key_2=input("Print the question ")
-                value_2=input("Print the answer ")
-                QUESTIONS.update({key_2:value_2})
-                print(QUESTIONS)
+                key=input("Print the question ")
+                value=input("Print the answer ")
+                QUESTIONS.update({key:value})
             else:
-                break
+                imp_question()
+                is_on=False
+        else:
+            imp_question()
+            is_on=False        
+def imp_question():
+    for item in QUESTIONS:
+        QuestionsForQuiz(item,QuestionsForQuiz[item])
 
-        
+def imp_answer():
+    pass
+QUESTIONS={}
 
 
 if __name__=="__main__":
