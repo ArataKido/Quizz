bf84a68bce9dd3a3f5a8cc2200a77b3bf5265035
Shahzod <shahzod20030905@gmail.com>
2022-03-24 09:48:40 +0100

    Changes

--- data.py
+++ data.py
@@ -1,12 +1,6 @@
 
 class Question: 
 
-    QUESTIONS={
-        "What is the capital of Finland": "lisabon",
-        "Who painted the Mona Lisa": "Leonardo da Vinci",
-        "What's a baby rabbit called?": "kittens",
-        "Which is the highest grossing Harry Potter film?": "Harry Potter and the Deathly Hallows Part 2'"
-    }    
     def __init__(self, question, answer) :
         self.question=question
         self.answer=answer
@@ -20,7 +14,15 @@ class Question:
         answer=self.QUESTIONS
         for i in answer:
             print(answer[i])
-   
-question=Question()
+
+QUESTIONS={
+    "What is the capital of Finland": "lisabon",
+    "Who painted the Mona Lisa": "Leonardo da Vinci",
+    "What's a baby rabbit called?": "kittens",
+    "Which is the highest grossing Harry Potter film?": "Harry Potter and the Deathly Hallows Part 2'"
+}    
+
+question=Question(QUESTIONS,"" )
+
 print(question.get_question())
 print(question.get_answer())
